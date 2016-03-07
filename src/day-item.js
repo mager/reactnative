3a9fddf95a3070f2144293ec52523d916e9c72fb
Andrew Mager <mager@postmates.com>
2016-03-06 20:40:24 -0800

    Dynamic styling

--- src/day-item.js
+++ src/day-item.js
@@ -1,27 +1,40 @@
 // Displays a single day of the week
 var React = require('react-native');
 var Text = React.Text;
-var StyleSheet = React.StyleSheet;
 
 // Create component
 var DayItem = React.createClass({
   render: function() {
     return (
-      <Text style={styles.day}>
+      <Text style={this.style()}>
         {this.props.day}
       </Text>
     );
+  },
+  style: function() {
+    return {
+      color: this.color(),
+      fontWeight: this.fontWeight(),
+      fontSize: this.fontSize(),
+      lineHeight: this.lineHeight()
+    }
+  },
+  color: function() {
+    var opacity = 1 / this.props.daysUntil;
+    return 'rgba(0, 0, 0, ' + opacity + ')';
+  },
+  fontWeight: function() {
+    var weight = (7 - this.props.daysUntil) * 100;
+    return weight.toString();
+  },
+  fontSize: function() {
+    return 60 - 6 * this.props.daysUntil;
+  },
+  lineHeight: function() {
+    return 70 - 4 * this.props.daysUntil;
   }
 });
 
-// Style component
-var styles = StyleSheet.create({
-  // Name the property whatever you want
-  day: {
-    fontSize: 18,
-    color: '#0000ff'
-  }
-});
 
 // Make this code available elsewhere
 module.exports = DayItem;
