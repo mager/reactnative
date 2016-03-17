daab25037b017a1f0c1e1f57771538ef6a83010a
Andrew Mager <mager@postmates.com>
2016-03-17 14:36:13 -0700

    MapView for weather

---
+++ weather/index.ios.js
@@ -0,0 +1,26 @@
+var React = require('react-native');
+var {
+  AppRegistry,
+  MapView,
+  View,
+  StyleSheet
+} = React;
+
+var Weather = React.createClass({
+  render: function() {
+    return <MapView
+      onRegionChangeComplete={this.onRegionChangeComplete}
+      style={styles.map}></MapView>
+  },
+  onRegionChangeComplete: function(region) {
+    console.log(region);
+  }
+});
+
+var styles = StyleSheet.create({
+  map: {
+    flex: 1
+  }
+});
+
+AppRegistry.registerComponent('weather', () => Weather);
